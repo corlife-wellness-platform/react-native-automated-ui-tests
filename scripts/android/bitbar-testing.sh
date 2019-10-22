b63e532c8ea0102cb978ee2b7c8fdd1327871235
Antonio Puleo <>
2019-10-22 14:51:23 +0100

    Removed app file id

--- scripts/android/bitbar-testing.sh
+++ scripts/android/bitbar-testing.sh
@@ -20,7 +20,6 @@ if [ ! -z "$BITBAR_API_KEY" ]
 
     echo "Uploading dummy application apk..."
     appFile=$(curl -X POST -u "$BITBAR_API_KEY": https://cloud.bitbar.com/api/me/files -F "file=@application.apk" | jq -r '.id')
-    appFile=129705060
 
     echo "Uploading test file..."
     testFile=$(curl -X POST -u "$BITBAR_API_KEY": https://cloud.bitbar.com/api/me/files -F "file=@bitbarTestsAndroid.zip" | jq -r '.id')
